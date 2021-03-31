cffc80c30237714000a07edda41f3c8ce9cf60bf
Nicky <nicky.dasmijn@gmail.com>
2021-03-31 22:51:40 +0200

    Autobuild artifacts.

---
+++ build_cmd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+exec 4>&1; export BASH_XTRACEFD=4; set -x
+# make errors fatal
+set -e
+# complain about unreferenced env variables
+set -u
+
+TRACY_VERSION="0.7.6"
+stage="$(pwd)/stage"
+echo "${TRACY_VERSION}" > "${stage}/VERSION.txt"
+
+mkdir -p "${stage}/include/tracy"
+cp -R tracy/common "${stage}/include/tracy"
+cp -R tracy/client "${stage}/include/tracy"
+cp -R tracy/libbacktrace "${stage}/include/tracy"
+
+mkdir -p "${stage}/LICENSES"
+cp tracy/LICENSE "${stage}/LICENSES/Tracy.txt"
